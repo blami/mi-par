c56cab6f5e0265e3d4fe4bb3d0e07ca214fae8e1
Ondrej Balaz <ondra@blami.net>
2010-10-06 15:24:06 +0200

    Added dump_unserialize method. Fixed dumping issues. 'srpgen' now takes optional output file argument.

--- srpdump.h
+++ srpdump.h
@@ -12,8 +12,9 @@
 #define __SRPDUMP_H
 
 
-void    dump_task(FILE *f, task_t *t);
-void    dump_board(FILE *f, task_t *t);
-void    dump_serialize(FILE *f, task_t *t);
+void        dump_task(FILE *f, task_t *t);
+void        dump_board(FILE *f, task_t *t);
+void        dump_serialize(FILE *f, task_t *t);
+task_t *    dump_unserialize(FILE *f);
 
 #endif /* __SRPDUMP_H */

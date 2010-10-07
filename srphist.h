00a5fcca4f85f776e804e2ff2bb13bc730b42638
Ondrej Balaz <ondra@blami.net>
2010-10-07 08:13:44 +0200

    Added function for move history lookups.

--- srphist.h
+++ srphist.h
@@ -10,7 +10,7 @@
  */
 #ifndef __SRPHIST_H
 #define __SRPHIST_H
-
+#include "srputils.h"
 
 #define FROM    0
 #define TO      1
@@ -28,8 +28,9 @@ typedef struct {
 	int l;                      // delka historie tahu
 } hist_t;
 
-hist_t *    hist_init(const hist_t *ho);
-void        hist_destroy(hist_t *h);
-void        hist_move(hist_t *h, move_t m);
+extern hist_t *    hist_init(const hist_t *ho);
+extern void        hist_destroy(hist_t *h);
+extern void        hist_append_move(hist_t *h, move_t m);
+extern coords_t    hist_lookup_move(hist_t *h, const int d, const unsigned int a);
 
 #endif /* __SRPHIST_H */

65fb873b9636c2f5cdea014724743747e7a8f258
Ondrej Balaz <ondra@blami.net>
2010-11-09 02:54:39 +0100

    Added debugging macros (better debugging on multiple MPI nodes). Added selective debugging of code components (DEBUG_COMP).

---
+++ srpprint.h
@@ -0,0 +1,58 @@
+/*
+ * MI-PAR: Paralelni systemy a algoritmy
+ * Semestralni uloha: SRP
+ * Autori: Ondrej Balaz <balazond@fit.cvut.cz>
+ *         Jan Mares <maresj16@fit.cvut.cz>
+ */
+
+/*
+ * srpprint: vypisovaci makra pro lazeni/sledovani vystupu
+ */
+#ifndef __SRPPRINT_H
+#define __SRPPRINT_H
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * Rizeni ladeni.
+ */
+#define DEBUG
+#define DEBUG_COMP "dump hist stack task utils mpi nompi gen"
+
+#define NONE -1                             /* don't use node */
+
+/*
+ * Funkce vypise zformatovany radek fmt z node n do souboru f.
+ */
+#define srpfprintf(f, n, fmt, ...) {                            \
+	if(n >= 0) {                                                \
+		fprintf(f, "[%d] %s:%03d %s(): " fmt "\n",              \
+			n, __FILE__, __LINE__, __func__, ##__VA_ARGS__);    \
+	} else {                                                    \
+		fprintf(f, "%s:%03d %s(): " fmt "\n",                   \
+			__FILE__, __LINE__, __func__, ##__VA_ARGS__);       \
+	}                                                           \
+}
+
+/**
+ * Funkce vypise zformatovany radek fmt z node n na standratni vystup.
+ */
+#define srpprintf(n, fmt, ...) {                                \
+	srpfprintf(stdout, n, fmt, ##__VA_ARGS__);                  \
+}
+
+/**
+ * Ladici makro.
+ */
+#ifdef DEBUG
+#define srpdebug(comp, n, fmt, ...) {                           \
+	if(strstr(DEBUG_COMP, comp) != NULL) {                      \
+		srpfprintf(stderr, n, "DEBUG-%s: " fmt,                 \
+			comp, ##__VA_ARGS__);                               \
+	}                                                           \
+}
+#else
+#define srpdebug(comp, n, fmt, ...)
+#endif /* DEBUG */
+
+#endif /* __SRPPRINT_H */

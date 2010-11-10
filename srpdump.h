00e9e2aa66dadd735fcb0d7d033bde802f5a885f
Ondrej Balaz <ondra@blami.net>
2010-11-10 01:53:16 +0100

    Migrated to debugging macros.

--- srpdump.h
+++ srpdump.h
@@ -14,11 +14,12 @@
 #include "srputils.h"
 #include "srphist.h"
 
+extern int node;    /* srpnompi.c/srpmpi.c */
 
-extern void        dump_serialize(FILE *f, task_t *t);
-extern task_t *    dump_unserialize(FILE *f);
-extern void        dump_task(FILE *f, task_t *t);
-extern void        dump_board(FILE *f, task_t *t, coords_t *B);
-extern void        dump_hist(FILE *f, hist_t *h);
+extern void         dump_serialize(FILE *f, task_t *t);
+extern task_t *     dump_unserialize(FILE *f);
+extern void         dump_task(FILE *f, task_t *t);
+extern void         dump_board(FILE *f, task_t *t, coords_t *B);
+extern void         dump_hist(FILE *f, hist_t *h);
 
 #endif /* __SRPDUMP_H */

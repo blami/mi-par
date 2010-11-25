911ece4fd888a23bc1aea0c17dea9cb269c19b9d
Ondrej Balaz <ondra@blami.net>
2010-11-25 01:23:17 +0100

    Fixed CPUs number in ethernet scheduler shell-script.

--- sched_ompi.sh
+++ sched_ompi.sh
@@ -2,17 +2,17 @@
 #sched_serial.sh: schedule all instances for OpenMPI run
 
 QSUB=qsub
-INST="inst1-5-5-5.srp"
-#	inst1-6-21-6.srp
-#	inst1-7-7-7.srp"
+INST="inst1-5-5-5.srp
+	inst1-6-21-6.srp
+	inst1-7-7-7.srp"
 
-CPU="2 4" # 8 16 24"
+CPU="2 4 8 16 24 32"
 
 for N in $CPU
 do
 	for I in $INST
 	do
-		#qsub -pe ompi $N -q default.q -l pdq ./qsub_ompi_eth.sh "$I"
+		qsub -pe ompi $N -q default.q -l pdq ./qsub_ompi_eth.sh "$I"
 		qsub -pe ompi $N -q default.q -l pdq ./qsub_ompi_inf.sh "$I"
 	done
 done
